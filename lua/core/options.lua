625145419e5b2f5123db01dd00cc5e580b1a465d
口x口 <kly777777777@gmail.com>
2025-11-16 22:25:06 +0800

    up

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -27,9 +27,8 @@ opt.smartcase = true
 --
 opt.termguicolors = true
 opt.signcolumn = "yes"
-
 -- Shell 配置
-opt.shell = "powershell"  -- 设置默认 shell 为 PowerShell
-opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"  -- PowerShell 参数
-opt.shellquote = ""  -- 引号设置
-opt.shellxquote = ""  -- 外部引号设置
+opt.shell = "powershell"                                                          -- 设置默认 shell 为 PowerShell
+opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"    -- PowerShell 参数
+opt.shellquote = ""                                                               -- 引号设置
+opt.shellxquote = ""                                                              -- 外部引号设置

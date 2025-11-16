684187701e1a2387fecb9649a0472ad458bb0839
kly777 <kly777777777@gmail.com>
2025-11-16 16:14:38 +0800

    add theme

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,31 +1,41 @@
-vim.g.mapleader=" "
+vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 local keymap = vim.keymap
 
---
-keymap.set("i","jk","<ESC>")
+-- === 基础快捷键 ===
 
+-- 插入模式退出到普通模式
+keymap.set("i", "jk", "<ESC>", { desc = "退出插入模式" })
 
-keymap.set("n","<TAB>",":bn<CR>")
-keymap.set("n","<S-TAB>",":bp<CR>")
+-- 标签页导航
+keymap.set("n", "<TAB>", ":bn<CR>", { desc = "下一个缓冲区" })
+keymap.set("n", "<S-TAB>", ":bp<CR>", { desc = "上一个缓冲区" })
 
--- 文件树快捷键
-keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+-- === 文件树快捷键 ===
 
--- LSP 快捷键
-keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
-keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
-keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
-keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
-keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
-keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
-keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
-keymap.set("n", "<leader>f", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>")
-keymap.set("n", "<leader>d", "<cmd>lua vim.diagnostic.open_float()<CR>")
-keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
-keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>")
+-- 切换文件树显示/隐藏
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "切换文件树" })
 
+-- === LSP 快捷键 ===
 
+-- 代码导航
+keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { desc = "跳转到定义" })
+keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { desc = "跳转到声明" })
+keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", { desc = "查看引用" })
+keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", { desc = "跳转到实现" })
 
+-- 代码信息
+keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { desc = "显示悬停文档" })
 
+-- 代码操作
+keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", { desc = "重命名符号" })
+keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", { desc = "代码操作" })
+
+-- 代码格式化
+keymap.set("n", "<leader>f", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", { desc = "格式化代码" })
+
+-- 诊断信息
+keymap.set("n", "<leader>d", "<cmd>lua vim.diagnostic.open_float()<CR>", { desc = "显示诊断信息" })
+keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { desc = "上一个诊断" })
+keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", { desc = "下一个诊断" })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,29 @@
 return {
+  -- 高亮方案
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        -- 可选配置，使用默认设置
+        style = "night", -- 可选: storm, night, day
+        transparent = false,
+        terminal_colors = true,
+        styles = {
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = {},
+          variables = {},
+          sidebars = "dark",
+          floats = "dark",
+        },
+      })
+      -- 设置默认高亮方案
+      vim.cmd("colorscheme tokyonight")
+    end,
+  },
+
   -- 文件树插件
   {
     "nvim-tree/nvim-tree.lua",

e45ec690231b5c18801ad4961514e13cf844edf9
kly777 <kly777777777@gmail.com>
2025-11-16 15:59:58 +0800

    add lsp

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -13,6 +13,19 @@ keymap.set("n","<S-TAB>",":bp<CR>")
 -- 文件树快捷键
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
 
+-- LSP 快捷键
+keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+keymap.set("n", "<leader>f", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>")
+keymap.set("n", "<leader>d", "<cmd>lua vim.diagnostic.open_float()<CR>")
+keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
+keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>")
+
 
 
 
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -28,4 +28,149 @@ return {
     "nvim-tree/nvim-web-devicons",
     lazy = true,
   },
+
+  -- LSP 配置
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+  },
+
+  -- Mason - LSP 管理器
+  {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate",
+    opts = {
+      ensure_installed = {
+        -- Lua
+        "lua_ls",
+        -- Python
+        "pyright",
+        -- JavaScript/TypeScript
+        "tsserver",
+        -- Go
+        "gopls",
+        -- Rust
+        "rust_analyzer",
+        -- C/C++
+        "clangd",
+        -- Java
+        "jdtls",
+        -- HTML/CSS/JSON
+        "html",
+        "cssls",
+        "jsonls",
+        -- Markdown
+        "marksman",
+        -- Shell
+        "bashls",
+        -- Docker
+        "dockerls",
+        -- YAML
+        "yamlls",
+      },
+    },
+    config = function(_, opts)
+      require("mason").setup(opts)
+      require("mason-lspconfig").setup({
+        automatic_installation = true,
+      })
+    end,
+  },
+
+  -- Mason LSP 配置
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
+  },
+
+  -- 自动补全
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+    end,
+  },
+
+  -- LSP 增强 UI
+  {
+    "glepnir/lspsaga.nvim",
+    event = "LspAttach",
+    config = function()
+      require("lspsaga").setup({
+        lightbulb = {
+          enable = false,
+        },
+      })
+    end,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
+
+  -- LSP 进度显示
+  {
+    "j-hui/fidget.nvim",
+    tag = "legacy",
+    event = "LspAttach",
+    config = function()
+      require("fidget").setup({})
+    end,
+  },
 }

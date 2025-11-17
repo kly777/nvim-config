5ff82e0993c8e82e1394e3428c97d3510e9e0a21
口x口 <kly777777777@gmail.com>
2025-11-17 19:55:30 +0800

    change theme

--- lua/plugins/configs/colorscheme.lua
+++ lua/plugins/configs/colorscheme.lua
@@ -1,26 +1,15 @@
 return {
         -- 高亮方案
         {
-                "folke/tokyonight.nvim",
+                "rebelot/kanagawa.nvim",
                 lazy = false,
                 priority = 1000,
                 config = function()
-                        require("tokyonight").setup({
-                                -- 可选配置，使用默认设置
-                                style = "night", -- 可选: storm, night, day
-                                transparent = false,
-                                terminal_colors = true,
-                                styles = {
-                                        comments = { italic = true },
-                                        keywords = { italic = true },
-                                        functions = { bold = true },
-                                        variables = {},
-                                        sidebars = "dark",
-                                        floats = "dark",
-                                },
+                        require("kanagawa").setup({
+                                theme = "dragon"
                         })
                         -- 设置默认高亮方案
-                        vim.cmd("colorscheme tokyonight")
+                        vim.cmd("colorscheme kanagawa")
                 end,
         },
 }

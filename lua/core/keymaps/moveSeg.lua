703fda7ab843c0ef49ac5c137f3bd390c9fcaa08
口x口 <kly777777777@gmail.com>
2026-05-12 11:54:53 +0800

    update

--- lua/core/keymaps/moveSeg.lua
+++
@@ -1,5 +0,0 @@
-local keymap = vim.keymap
-
--- 在可视模式下移动选中的代码段
-keymap.set("v", "<C-J>", ":m '>+1<CR>gv=gv", { desc = "向下移动选中代码段" })
-keymap.set("v", "<C-K>", ":m '<-2<CR>gv=gv", { desc = "向上移动选中代码段" })
